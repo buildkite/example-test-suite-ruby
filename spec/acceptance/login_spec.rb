dd1a0066a9ff5f92c0a74ac592fb838d8d6328fa
James Hill <395+jameshill@users.noreply.github.com.>
2024-04-22 10:36:07 +1000

    Here is a PR that will fail

--- spec/acceptance/login_spec.rb
+++ spec/acceptance/login_spec.rb
@@ -1,13 +1,17 @@
 require "spec_helper"
 
 describe "Login" do
-  25.times do
+  25.times do |run|
     it "should authenticate users" do
       expect(2).to eq(2)
     end
 
     it "should restrict access" do
-      expect(2).to eq(2)
+      if run % 22 == 0
+        expect(1).to eq(2)
+      else
+        expect(2).to eq(2)
+      end
     end
   end
 end
